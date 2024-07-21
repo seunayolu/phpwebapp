8598515c2bad34ae88ce53617cee9e54b55469da
seunayolu <seunayolu@gmail.com>
2024-07-21 23:28:25 +0100

    parameter store credentails updated

--- parameter_store/fetch_credentials.php
+++ parameter_store/fetch_credentials.php
@@ -6,7 +6,7 @@ use Aws\Ssm\SsmClient;
 function getParameter($name) {
     $client = new SsmClient([
         'version' => 'latest',
-        'region'  => 'your-region' // e.g., 'us-west-2'
+        'region'  => 'eu-west-1' // e.g., 'us-west-2'
     ]);
 
     $result = $client->getParameter([
@@ -17,8 +17,8 @@ function getParameter($name) {
     return $result['Parameter']['Value'];
 }
 
-$db_host = getParameter('/myapp/DB_HOST');
-$db_username = getParameter('/myapp/DB_USERNAME');
-$db_password = getParameter('/myapp/DB_PASSWORD');
-$db_name = getParameter('/myapp/DB_NAME');
+$db_host = getParameter('/phpwebapp/db_host');
+$db_username = getParameter('/phpwebapp/db_username');
+$db_password = getParameter('/phpwebapp/db_password');
+$db_name = getParameter('/phpwebapp/db_name');
 ?>
