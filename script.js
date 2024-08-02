c5779800ab6aa3bdd0cffa0490a9baa53d1750ea
seunayolu <seunayolu@gmail.com>
2024-08-03 00:09:34 +0100

    php contact form added to a new branch

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let formData = new FormData(this);
+
+    fetch('process.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+        alert(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
